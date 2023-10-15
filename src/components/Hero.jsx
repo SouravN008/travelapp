9f2d431b0dca725636469a05e7e3804b2f6d3131
SouravN008 <121627237+SouravN008@users.noreply.github.com>
2023-10-15 22:48:27 +0530

    my travel app using Tailwindcss

---
+++ src/components/Hero.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <img src="https://images.unsplash.com/photo-1501785888041-af3ef285b470?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=2070&q=80" alt="/" />
+      <div>
+       <div>
+           <h1>Choose Your special Trip</h1>
+           <h2>With Mytravel</h2>
+           <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque ut deleniti cupiditate quisquam perspiciatis. Accusantium ipsam sunt corporis ad illum non repudiandae, quaerat quo nam porro commodi rem eos. Mollitia?</p>
+       </div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
